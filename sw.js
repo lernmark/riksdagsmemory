ab407cc175081d06138f7728e950bd4a5a32b422
Paul Lernmark <paul@lernmark.net>
2026-04-18 08:15:35 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'rd-memory-v1';
+const ASSETS = [
+  'index.html',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/vue@3/dist/vue.global.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
